c9abc9f3b1cb729fa96963ceeb897ba5dc863365
Matej Buljan <matejb6@icloud.com>
2022-10-06 08:41:00 +0200

    Add main file

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+class Main {
+  /**
+   * @public
+   * @static
+   * @description Main run method, entry point to program execution
+   */
+  public static run(): void {
+    console.log('Hello World');
+  }
+}
+
+Main.run();
