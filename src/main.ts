b2628e370bef80a85473d40cdacbd67a14209b79
Matej Buljan <matejb6@icloud.com>
2024-11-14 12:02:12 +0100

    Use calc class in main execution

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { Calc } from './shared/calc.ts';
+
 class Main {
   /**
-   * Main run method, entry point to program execution
+   * Main run method, entry point for execution
    */
   public static run(): void {
     console.log('Hello World');
+
+    const a = 2;
+    const b = 3;
+    const result = Calc.add(2, 3);
+    console.log(`Adding ${a} and ${b} results in ${result}`);
   }
 }
 
